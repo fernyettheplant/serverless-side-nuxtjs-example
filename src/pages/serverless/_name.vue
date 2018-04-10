f8ae5243b56e105220a5eca33090c29a1426a30f
Jesús Fernando Alvarez Franco <jefer_590@hotmail.com>
2018-04-10 00:25:01 -0500

    :art: - Add Page with Param

---
+++ src/pages/serverless/_name.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>Hey, I think your name is {{ name }} 👋</h2>
+</template>
+
+<script>
+  export default {
+    layout: 'serverless',
+
+    data () {
+      return {
+        name: this.$route.params.name
+      }
+    },
+
+    head () {
+      return {
+        title: `${this.name} Person`,
+        meta: [
+          { hid: 'description', name: 'description', content: `You are ${this.name} hello 👋` }
+        ]
+      }
+    }
+  }
+</script>
